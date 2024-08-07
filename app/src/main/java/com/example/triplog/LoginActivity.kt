5be00340d985731987141d77315f41c0d22fc273
adrnn23 <adrianoblizajek1@gmail.com>
2024-08-07 15:36:22 +0200

    login, registration

--- app/src/main/java/com/example/triplog/LoginActivity.kt
+++ app/src/main/java/com/example/triplog/LoginActivity.kt
@@ -2,14 +2,16 @@ package com.example.triplog
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,11 +21,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.triplog.ui.theme.TripLogTheme
 
 @Composable
 fun LoginScreen(navController: NavController) {
@@ -41,39 +50,43 @@ fun LoginScreen(navController: NavController) {
             verticalArrangement = Arrangement.Top
         ) {
             Text(
-                color = MaterialTheme.colorScheme.onSecondary,
                 text = stringResource(R.string.app_name),
                 fontSize = 64.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier
                     .padding(bottom = 20.dp, top = 60.dp)
             )
-            Text(
-                color = MaterialTheme.colorScheme.onSecondary,
-                text = stringResource(R.string.logowanie),
-                fontSize = 32.sp,
-                modifier = Modifier
-                    .padding(bottom = 10.dp, top = 120.dp)
-            )
         }
 
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
+            horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Center,
-            modifier = Modifier.padding(10.dp)
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
         ) {
+            Text(
+                text = stringResource(R.string.logowanie),
+                fontSize = 32.sp,
+                modifier = Modifier
+                    .padding(top = 10.dp, bottom = 10.dp)
+            )
 
-            UsernameEmailInput(
+            EmailInput(
                 label = R.string.email,
                 value = email,
                 onValueChanged = { email = it },
-                modifier = Modifier.padding(5.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, bottom = 10.dp)
             )
             PasswordInput(
                 label = R.string.password,
                 value = password,
                 onValueChanged = { password = it },
-                modifier = Modifier.padding(5.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, bottom = 10.dp)
             )
         }
         Column(
@@ -83,7 +96,11 @@ fun LoginScreen(navController: NavController) {
                 .padding(10.dp)
         ) {
             LoginButton()
-            Spacer(modifier = Modifier.height(30.dp))
+
+            Spacer(modifier = Modifier.height(10.dp))
+            Divider(color = Color.DarkGray, thickness = 1.dp)
+            Spacer(modifier = Modifier.height(10.dp))
+
             NoAccount(navController)
         }
 
@@ -92,44 +109,53 @@ fun LoginScreen(navController: NavController) {
 
 @Composable
 fun NoAccount(navController: NavController) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top,
-    ) {
-        Text(
-            color = MaterialTheme.colorScheme.onSecondary,
-            text = stringResource(id = R.string.noAccount),
-            fontSize = 16.sp,
-            modifier = Modifier
-                .padding(bottom = 10.dp, top = 10.dp)
+
+    val annotatedString = buildAnnotatedString {
+        val str = "Nie masz konta? Zarejestruj się!"
+        val startStr = str.indexOf("Zarejestruj")
+        val endStr = str.indexOf("!")
+
+        append(str)
+        addStyle(
+            style = SpanStyle(
+                color = Color(0, 0, 128),
+                textDecoration = TextDecoration.Underline,
+            ),
+            start = startStr, end = endStr
         )
-        Button(
-            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
-            onClick = { navController.navigate(Screen.RegistrationScreen.destination) }
-        ) {
-            Text(
-                color = MaterialTheme.colorScheme.onPrimaryContainer,
-                text = stringResource(R.string.register),
-                fontSize = 14.sp,
-                modifier = Modifier
-                    .padding(2.dp)
-            )
-        }
     }
+
+    ClickableText(
+        text = annotatedString,
+        onClick = { navController.navigate(Screen.RegistrationScreen.destination) },
+        modifier = Modifier
+            .padding(2.dp)
+    )
+
 }
 
 @Composable
 fun LoginButton() {
     Button(
         colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
-        onClick = { /*TODO*/ }
+        onClick = { /*TODO*/ },
+        modifier = Modifier.width(220.dp)
     ) {
         Text(
             color = MaterialTheme.colorScheme.onPrimaryContainer,
             text = stringResource(R.string.login),
-            fontSize = 14.sp,
+            fontSize = 18.sp,
             modifier = Modifier
                 .padding(2.dp)
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    TripLogTheme {
+        val navController = rememberNavController()
+        LoginScreen(navController = navController)
     }
 }
--- app/src/main/java/com/example/triplog/RegistrationActivity.kt
+++ app/src/main/java/com/example/triplog/RegistrationActivity.kt
@@ -1,15 +1,25 @@
 package com.example.triplog
 
+import android.content.Context
+import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material.icons.filled.Password
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -20,13 +30,32 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.triplog.ui.theme.TripLogTheme
+
+
+fun registrationValidation(
+    username: String, email: String,
+    password: String, repeatedPassword: String, context:Context
+): Boolean {
+    if (username == "" || email == "" || password == "" || repeatedPassword == "") {
+        Toast.makeText(context, "Uzupełnij wymagane pola.", Toast.LENGTH_SHORT).show()
+        return true
+    }
+    Toast.makeText(context, "Rejestracja...", Toast.LENGTH_SHORT).show()
+    return false
+}
+
 
 @Composable
 fun RegistrationScreen(navController: NavController) {
@@ -46,50 +75,56 @@ fun RegistrationScreen(navController: NavController) {
             verticalArrangement = Arrangement.Top
         ) {
             Text(
-                color = MaterialTheme.colorScheme.onSecondary,
                 text = stringResource(R.string.app_name),
                 fontSize = 64.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier
                     .padding(bottom = 20.dp, top = 60.dp)
             )
-            Text(
-                color = MaterialTheme.colorScheme.onSecondary,
-                text = stringResource(R.string.rejestracja),
-                fontSize = 32.sp,
-                modifier = Modifier
-                    .padding(bottom = 10.dp, top = 120.dp)
-            )
         }
 
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
+            horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.padding(10.dp)
         ) {
-            UsernameEmailInput(
+            Text(
+                text = stringResource(R.string.rejestracja),
+                fontSize = 32.sp,
+                modifier = Modifier
+                    .padding(bottom = 10.dp, top = 10.dp)
+            )
+            UsernameInput(
                 label = R.string.username,
                 value = username,
                 onValueChanged = { username = it },
-                modifier = Modifier.padding(5.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, bottom = 10.dp)
             )
-            UsernameEmailInput(
+            EmailInput(
                 label = R.string.email,
                 value = email,
                 onValueChanged = { email = it },
-                modifier = Modifier.padding(5.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, bottom = 10.dp)
             )
             PasswordInput(
                 label = R.string.password,
                 value = password,
                 onValueChanged = { password = it },
-                modifier = Modifier.padding(5.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, bottom = 10.dp)
             )
             PasswordInput(
                 label = R.string.repeatedPassword,
                 value = repeatedPassword,
                 onValueChanged = { repeatedPassword = it },
-                modifier = Modifier.padding(5.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, bottom = 10.dp)
             )
         }
         Column(
@@ -98,8 +133,12 @@ fun RegistrationScreen(navController: NavController) {
             modifier = Modifier
                 .padding(10.dp)
         ) {
-            RegisterButton()
+            RegisterButton(username, email, password, repeatedPassword)
+
             Spacer(modifier = Modifier.height(10.dp))
+            Divider(color = Color.DarkGray, thickness = 1.dp)
+            Spacer(modifier = Modifier.height(10.dp))
+
             BackToLoginButton(navController)
         }
 
@@ -107,15 +146,20 @@ fun RegistrationScreen(navController: NavController) {
 }
 
 @Composable
-fun RegisterButton() {
+fun RegisterButton(username: String, email: String, password: String, repeatedPassword: String) {
+    var emptyRegistrationFields by remember { mutableStateOf(false) }
+    val context: Context = LocalContext.current
     Button(
         colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
-        onClick = { /*TODO*/ }
+        onClick = {
+            emptyRegistrationFields = registrationValidation(username, email, password, repeatedPassword, context)
+        },
+        modifier = Modifier.width(240.dp)
     ) {
         Text(
             color = MaterialTheme.colorScheme.onPrimaryContainer,
             text = stringResource(R.string.register),
-            fontSize = 14.sp,
+            fontSize = 18.sp,
             modifier = Modifier
                 .padding(2.dp)
         )
@@ -126,12 +170,14 @@ fun RegisterButton() {
 fun BackToLoginButton(navController: NavController) {
     Button(
         colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
-        onClick = { navController.navigate(Screen.LoginScreen.destination) }
+        onClick = { navController.navigate(Screen.LoginScreen.destination) },
+        modifier = Modifier
+            .width(240.dp)
     ) {
         Text(
             color = MaterialTheme.colorScheme.onPrimaryContainer,
-            text = stringResource(R.string.backToLogin),
-            fontSize = 14.sp,
+            text = stringResource(R.string.login),
+            fontSize = 18.sp,
             modifier = Modifier
                 .padding(2.dp)
         )
@@ -139,16 +185,51 @@ fun BackToLoginButton(navController: NavController) {
 }
 
 @Composable
-fun UsernameEmailInput(
+fun UsernameInput(
     @StringRes label: Int,
     value: String,
     onValueChanged: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val usernameIcon = @Composable {
+        Icon(
+            Icons.Default.Person,
+            contentDescription = "userIcon",
+            tint = MaterialTheme.colorScheme.primary
+        )
+    }
     TextField(
         label = { Text(stringResource(label)) },
         value = value,
         onValueChange = onValueChanged,
+        leadingIcon = usernameIcon,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next
+        ),
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun EmailInput(
+    @StringRes label: Int,
+    value: String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val emailIcon = @Composable {
+        Icon(
+            Icons.Default.Mail,
+            contentDescription = "mailIcon",
+            tint = MaterialTheme.colorScheme.primary
+        )
+    }
+    TextField(
+        label = { Text(stringResource(label)) },
+        value = value,
+        onValueChange = onValueChanged,
+        leadingIcon = emailIcon,
         singleLine = true,
         keyboardOptions = KeyboardOptions(
             imeAction = ImeAction.Next
@@ -165,15 +246,32 @@ fun PasswordInput(
     onValueChanged: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val passwordIcon = @Composable {
+        Icon(
+            Icons.Default.Password,
+            contentDescription = "passwordIcon",
+            tint = MaterialTheme.colorScheme.primary
+        )
+    }
     TextField(
         label = { Text(stringResource(label)) },
         value = value,
         onValueChange = onValueChanged,
         singleLine = true,
+        leadingIcon = passwordIcon,
         keyboardOptions = KeyboardOptions(
             imeAction = ImeAction.Next
         ),
         modifier = modifier,
         visualTransformation = PasswordVisualTransformation()
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegistrationScreenPreview() {
+    TripLogTheme {
+        val navController = rememberNavController()
+        RegistrationScreen(navController = navController)
+    }
 }
